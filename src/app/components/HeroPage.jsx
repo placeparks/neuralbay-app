224b06889f3eb2271fa7983809362e40d32f7f6f
placeparks <placeparks@gmail.com>
2024-04-27 20:07:31 +0500

    bg added

--- src/app/components/HeroPage.jsx
+++ src/app/components/HeroPage.jsx
@@ -23,13 +23,13 @@ const Hero = () => {
     }, []);
 
   return (
-    <div className='md:h-screen h-[110vh] relative -mt-20 overflow-hidden'>
-    <video autoPlay muted loop className='videoOverlay z-0 opacity-40'>
+    <div className='bgimage md:h-screen h-[110vh] relative -mt-20 overflow-hidden'>
+      <video autoPlay muted loop className='videoOverlay z-0 opacity-40 mix-blend-exclusion'>
         <source src="/assets/vidhero.mp4" type="video/mp4" />
-    </video>
+      </video>
     <section className="z-10 absolute inset-0 flex items-center justify-center">
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-          <div className="pt-20 md:pt-0 col-span-3 mr-auto place-self-center lg:col-span-7">
+          <div className="pt-20 md:pt-0 col-span-3 mr-auto place-self-center lg:col-span-6">
           <p className='text-transparent text-1xl font-medium gradient-text bg-gradient-to-r from-customPurple-light to-customPurple-dark to-customPurple-dark animate-gradient tracking-widest'>WELCOME TO NEURALBAY</p>
                       <h1 className=" max-w-2xl mb-4 text-3xl font-extrabold tracking-tight text-white md:text-5xl xl:text-5xl poppins-extrabold" style={style}>
     Discover, Experiment, Buy & Sell Pretrained <span className='text-transparent bg-gradient-to-r from-customBlue-light to-customBlue-dark to-customBlue-dark gradient-text2'>AI</span> Models
@@ -40,7 +40,7 @@ const Hero = () => {
               <ButtonFill2 title='Create' />
          </div>
           </div>
-          <div className="flex lg:col-span-5 justify-center items-center lg:mt-0">
+          <div className="flex lg:col-span-6 justify-center items-center lg:mt-0">
     <Image src="/assets/girl.png" alt="mockup" width={800} height={900} className='z-30 ml-32 md:ml-0 h-60 md:h-auto'/>
 </div>
 
